8ccee8af8ab9d542eb20fdc1b9f4447e2586e328
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-06 13:23:08 -0600

    innerHtml y colocar clases al DOM desde la directiva

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -7,6 +7,7 @@ export class ErrorMsgDirective implements OnInit{
 
   htmlElement : ElementRef<HTMLElement>;
   @Input() color: string = 'red'
+  @Input() mensaje: string = 'Este campo es requirido'
 
   constructor( private el : ElementRef<HTMLElement>) {
     console.log('constructor directive');
@@ -18,10 +19,16 @@ export class ErrorMsgDirective implements OnInit{
   ngOnInit(): void {
     console.log('OnInit directive');    
     this.setColor();
+    this.setMensaje();
   }
 
   setColor():void{
     this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.classList.add('form-text');
+  }
+
+  setMensaje():void{
+    this.htmlElement.nativeElement.innerHTML = this.mensaje;
   }
 
 }
