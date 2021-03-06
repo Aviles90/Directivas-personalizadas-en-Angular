9b83c473cf55f55642b3c0be068bb6a678e02f08
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-06 12:04:36 -0600

    creación de formulario reactivo

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductosRoutingModule } from './productos-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 
 
 @NgModule({
-  declarations: [AgregarComponent],
+  declarations: [
+    AgregarComponent
+  ],
   imports: [
     CommonModule,
-    ProductosRoutingModule
+    ProductosRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductosModule { }
